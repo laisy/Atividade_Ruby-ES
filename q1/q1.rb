1320f08cc85443a8a747c875f31e1ecca44dc7ee
laisy <laisyferreira2011@gmail.com>
2021-01-02 21:13:56 -0300

    adicionando codigo da questão 1, funcionando e o arquivo txt de entrada

---
+++ q1/q1.rb
@@ -0,0 +1,39 @@
+class Class
+    def attr_accessor_with_history(name)
+        name = name.to_s        #transformar em string
+        attr_reader name
+        attr_reader name+"_history"
+
+        #classe que define metodos de instância
+        class_eval %Q"
+            def #{name}=(value)
+                if !defined? @#{name}_history
+                    @#{name}_history = [@#{name}]
+                end
+                @#{name} = value
+                @#{name}_history << value
+
+                if @#{name}_history.first == nil
+                    @#{name}_history.shift
+
+                end
+            end
+        "
+    end
+end
+
+class Foo
+    attr_accessor_with_history :bar
+    def history (name)
+        print bar_history
+    end
+end
+
+f = Foo.new         # => #<Foo:0x127e678>
+
+File.open('entradaq1.txt', 'r').each_line do |line|
+    line.slice! "\n"
+    f.bar = eval(line)
+end
+
+f.history(:bar)     # => [3, :wowzo, 'boo!']
